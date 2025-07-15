767b9af949519a7ffd73cd46b9a112a20c2f32ca
Ben Jordan <ben254378@gmail.com>
2025-07-15 14:34:55 -0700

    implemented polling for job results

--- notes-digitizer/src/js/main.js
+++ notes-digitizer/src/js/main.js
@@ -1,9 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+    // Copy Shortcut
+    document.getElementById('copy').addEventListener('click', (event) => {
+        navigator.clipboard.writeText(document.getElementById("output").textContent);
+    });
+
+    // File Selection
     document.getElementById('file').addEventListener('change', (event) => {
         document.getElementById('selectedFile').textContent = `${event.target.files[0].name} \u2713`;
     });
 
+    // File Submission
     document.getElementById('uploadForm').addEventListener('submit', async function (event) {
         event.preventDefault();
         try {
@@ -26,24 +33,6 @@ document.addEventListener('DOMContentLoaded', () => {
             // append the file to the form
             const FileInput = event.target.querySelector('input[type="file"]');
             form.append(FileInput.name, FileInput.files[0]);
-            
-            // use key to create polling entry
-            let card = document.createElement('p');
-            card.textContent = FileInput.files[0].name;
-            card.classList.add("inline");
-            let refresh = document.createElement('p');
-            refresh.textContent = " \u21bb";
-            refresh.classList.add("inline","cursor-pointer");
-            refresh.addEventListener('click', (event) => {
-                // make polling request
-                const key = data.fields.key;
-                
-            });
-            document.getElementById('jobs').append(card, refresh);
-            event.target.reset();
-            document.getElementById('selectedFile').textContent = "None Selected";
-
-            return;
 
             // submit the form to post image to s3 bucket
             const s3Response = await fetch(data.url, {
@@ -53,7 +42,47 @@ document.addEventListener('DOMContentLoaded', () => {
             if(s3Response.ok){
                 // use key to create polling entry
                 let card = document.createElement('p');
-                card.textContent = FileInput.name;
+                card.textContent = FileInput.files[0].name;
+                card.classList.add("cursor-pointer");
+                card.dataset.poll = "true";
+                card.addEventListener('click', async function (event) {
+                    const key = data.fields.key;
+                    const output = document.getElementById("output");
+                    // make polling request
+                    if(event.target.dataset.poll === "true"){
+                        const pollResponse = await fetch(`https://7y3hagl18l.execute-api.us-east-2.amazonaws.com/dev/poll?key=${key}`, {method: 'GET'})
+                        console.log(pollResponse);
+                        const body = await pollResponse.json();
+                        switch(pollResponse.status){
+                            case 200:
+                                output.textContent = body.data;
+                                localStorage.setItem(key, body.data);
+                                card.dataset.poll = "false";
+                                break;
+                            case 201:
+                                output.textContent = "PROCESSING, please try again";
+                                break;
+                            case 202:
+                                output.textContent = `FAILURE: ${body.data}`;
+                                localStorage.setItem(key, `FAILURE: ${body.data}`);
+                                card.dataset.poll = "false";
+                                break;
+                            case 404:
+                                output.textContent = "JOB NOT FOUND";
+                                localStorage.setItem(key, "JOB NOT FOUND");
+                                card.dataset.poll = "false";
+                                break;
+                            case 500:
+                                output.textContent = body.data;
+                                localStorage.setItem(key, body.data);
+                                card.dataset.poll = "false";
+                                break;
+                        }
+                    }else{
+                        // fetch from local storage
+                        output.textContent = localStorage.getItem(key);
+                    }
+                });
                 document.getElementById('jobs').append(card);
                 event.target.reset();
                 document.getElementById('selectedFile').textContent = "None Selected";
